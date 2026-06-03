807fe51100a7f83d3ac497628fa54eae356194f7
congMinh <minh.ptit.k6@gmail.com>
2026-06-04 01:28:00 +0700

    bật hasTotal cho bảng

--- src/components/Table/TableStaticData.tsx
+++ src/components/Table/TableStaticData.tsx
@@ -16,7 +16,7 @@ import { updateSearchStorage } from './utils';
 
 const TableStaticData = (props: TableStaticProps) => {
 	const intl = useIntl();
-	const { Form, showEdit, setShowEdit, addStt, data, children, hasCreate, hasTotal, rowSortable } = props;
+	const { Form, showEdit, setShowEdit, addStt, data, children, hasCreate, hasTotal = true, rowSortable } = props;
 	const { danhSach: dsPhanVung } = useModel('core.phanvungdulieu');
 	const [searchText, setSearchText] = useState<string>('');
 	const [searchedColumn, setSearchedColumn] = useState();
